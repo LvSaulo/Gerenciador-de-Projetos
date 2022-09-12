ac5493d8b26d6743c9b30e55d89f3b7dc5f2fb07
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-09-11 23:59:14 -0300

    Update Atividades.java

--- Atividades.java
+++ Atividades.java
@@ -6,14 +6,14 @@ public class Atividades {
     public String descricao;
     public String data_i, hora_i, data_t, hora_t;
     public String responsavel;
-    public String[] profissionais;
-    public String[][] tarefas_per_profissional;
+    public String profissionais;
+    public String[] tarefas_per_profissional;
 
     public int qntP;
-    public int[] tarefas;
+    public int tarefas;
 
     public Atividades(String identificacao, String descricao, String data_i, String hora_i, String data_t,
-            String hora_t, String responsavel, String[] profissionais, String[][] tarefas_per_profissional) {
+            String hora_t, String responsavel, String profissionais, String[] tarefas_per_profissional) {
         
         this.identificacao = identificacao;
         this.descricao = descricao;
@@ -26,54 +26,77 @@ public class Atividades {
         this.tarefas_per_profissional = tarefas_per_profissional;
     }
 
+    public String getId() {return this.identificacao;}
+    public String getDesc() {return this.descricao;}
+    public String getDataI() {return this.data_i;}
+    public String getHoraI() {return this.hora_i;}
+    public String getDataT() {return this.data_t;}
+    public String getHoraT() {return this.hora_t;}
+    public String getResponsavel() {return this.responsavel;}
+    public String getProfissional() {return this.profissionais;}
+    public String[] getTarefas() {return tarefas_per_profissional;}
+
+
+    Scanner in = new Scanner(System.in);
+    
+    public void desc() {
+        System.out.print("Descrição da Atividade: ");
+        this.descricao = in.next();
+    }
+    public void dataI() {
+        System.out.print("Digite a data de inicio: ");
+        this.data_i = in.next();
+    }
+    public void horaI() {
+        System.out.println("Digite a hora de inicio: ");
+        this.hora_i = in.next();
+    }
+    
+    public void dataT() {
+        System.out.println("Digite a data de termino: ");
+        this.data_t = in.next();
+    }
+    public void horaT() {
+        System.out.println("Digite a hora de termino: ");
+        this.hora_t = in.next();
+    }
+    public void responsavelA() {
+        System.out.println("Digite o nome do responsavel da Atividade: ");
+        this.responsavel = in.next();
+    }
+    public void profissional() {
+        System.out.println("Digite o nome do profissional: ");
+        this.profissionais = in.next();
+
+        System.out.println("Quantas atividades esse profissional vai exercer? ");
+        tarefas = in.nextInt();
+
+        taref(tarefas);
+    }
+    public void taref(int tarefas) {
+        for(int j = 0; j < tarefas; j++) {
+            System.out.printf("Digite o nome da atividade que o profissional %s vai fazer: ", this.profissionais);
+            this.tarefas_per_profissional[j] = in.next();
+        }
+    }
+
     public void addAtividades() {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Vamos começar a criação da Atividade!\n\n");
 
-        System.out.println("Digite a identificacao da Atividade: ");
-        this.identificacao = in.next();
+        desc();
+        dataI();
+        horaI();
+        dataT();
+        horaT();
+        responsavelA();
+        profissional();       
+              
+        /*System.out.println("Quantos profissionais vão participar do projeto? ");
+        qntP = in.nextInt(); // salva a quantidade de profissionais */
 
-     /* for( ) { // checa todas as identificacoes existentes
-            if(this.identificacao == ??) { // Ver se já existe alguma identificao para atividades igual
-                System.out.println("\nEssa identificao já existe, digite uma nova: ");
-                this.identificacao = in.next();
-            }
-        } */
-
-        System.out.print("Descrição da Atividade: ");
-        this.descricao = in.next();
-
-        System.out.print("Digite a data de inicio: ");
-        this.data_i = in.next();
-
-        System.out.println("Digite a hora de inicio: ");
-        this.hora_i = in.next();
-
-        System.out.println("Digite a data de termino: ");
-        this.data_t = in.next();
-
-        System.out.println("Digite a hora de termino: ");
-        this.hora_i = in.next();
-
-        System.out.println("Digite o nome do responsavel da Atividade: ");
-        this.responsavel = in.next();
-
-        System.out.println("Quantos profissionais vão participar do projeto? ");
-        qntP = in.nextInt(); // salva a quantidade de projetos
-
-        for(int i = 0; i < qntP; i++) {
-            System.out.println("Digite o nome do profissional: ");
-            this.profissionais[i] = in.next();
-
-            System.out.println("Quantas atividades esse profissional vai exercer? ");
-            tarefas[i] = in.nextInt();
-
-            for(int j = 0; j < tarefas[i]; j++) {
-                System.out.printf("Digite o nome da atividade que o profissional %s vai fazer: ", this.profissionais[i]);
-                this.tarefas_per_profissional[i][j] = in.next();
-            }
-        }
+        /*for(int i = 0; i < qntP; i++) {}/* */      
     }
 
     public void removeAtividades() {
