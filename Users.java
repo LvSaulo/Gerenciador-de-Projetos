6db2145d4ea6b51824cdf200b3ec1aa152540fbd
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-09-13 22:59:46 -0300

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,35 +1,58 @@
 import java.util.Scanner;
 
+
+
 public class Users {
 
     public int id;
     public String senha;
     public String name;
     public String ocupacao;
+    public double valor_bolsa;
+    public double tempo_bolsa; // em horas
 
-    public Users(int id, String name, String ocupacao, String senha) {
+    public Users(int id, String name, String senha, Double valor_bolsa, Double tempo_bolsa) {
         this.id = id;
         this.senha = senha;
         this.name = name;
-        this.ocupacao = ocupacao;
+        this.valor_bolsa = valor_bolsa;
+        this.tempo_bolsa = tempo_bolsa;
     }
 
-    
-    public void addUsers() {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Digite seu id: ");
-        this.id = input.nextInt();
+    public int getID() {return this.id;}
+    public String getSenha() {return this.senha;}
+    public String getName() {return this.name;}
+    public Double getValorBolsa() {return this.valor_bolsa;}
 
+    public void addUsers() {
+        setPassword();
+        setName();
+        setStatusUser();
+        setValorBolsa();
+        setTempoBolsa();
+    }
+
+    Scanner input = new Scanner(System.in);
+    public void setPassword() {
         System.out.println("Digite sua senha: ");
         this.senha = input.next();
-
+    }
+    public void setName() {
         System.out.println("Digite seu nome: ");
         this.name = input.next();
-
-        System.out.println("Digite sua ocupação: ");
-        this.ocupacao = input.next();
-
+    }
+    public void setStatusUser() {
+        System.out.println("Tipos de usuarios: Aluno, Professor, Pesquisador, Profissional, Tecnico.\n");
+        System.out.println("Digite seu tipo de usuario: ");
+        StatusUser status = StatusUser.valueOf(input.nextLine().toUpperCase());
+    }
+    public void setValorBolsa() {
+        System.out.println("Digite o valor da bolsa: ");
+        this.valor_bolsa = input.nextDouble();
+    }
+    public void setTempoBolsa() {
+        System.out.println("Digite o tempo da bolsa: ");
+        this.tempo_bolsa = input.nextDouble();
     }
 
     public void removeUsers() {
@@ -40,6 +63,7 @@ public class Users {
 
     }
 
+    
     public void editUsers() {
         Scanner input = new Scanner(System.in);
 
@@ -66,7 +90,7 @@ public class Users {
             }
             else if(edit.equalsIgnoreCase("ocupacao")) {
                 System.out.println("\nDigite sua nova ocupacao: ");
-                this.ocupacao = input.next();
+                StatusUser status = StatusUser.valueOf(input.nextLine().toUpperCase());
             } else {
                 System.out.println("\nDigite uma opção válida!\n");
                 i = true;
