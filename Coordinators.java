854641694da778f8f714d33dab4cb8e8dc34fbec
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-07 18:01:47 -0300

    Create Coordinators.java

---
+++ Coordinators.java
@@ -0,0 +1,7 @@
+public class Coordinators extends Users{
+    public Coordinators(int id, String name, String password) {
+        super(id, name, password);
+
+        Users user = new Users();
+    }
+}
