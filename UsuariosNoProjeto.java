4c5cb8fb1152650ff24b8961d53361e6e239b6e4
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-07 18:08:27 -0300

    Create UsuariosNoProjeto.java

---
+++ UsuariosNoProjeto.java
@@ -0,0 +1,9 @@
+public class UsuariosNoProjeto {
+    public int id;
+    public String name;
+    public String ocupacao;
+
+    public UsuariosNoProjeto(int id, String name, String ocupacao) {
+        
+    }
+}
