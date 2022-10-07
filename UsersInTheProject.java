a32470c7988b145aec4c8fd6c7fd9f585ea9eeb9
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-07 18:30:57 -0300

    Update and rename UsuariosNoProjeto.java to UsersInTheProject.java

---
+++ UsersInTheProject.java
@@ -0,0 +1,9 @@
+public class UsersInTheProject {
+    public int id;
+    public String name;
+    public String ocupacao;
+
+    public UsersInTheProject(int id, String name, String ocupacao) {
+        
+    }
+}
--- UsuariosNoProjeto.java
+++
@@ -1,9 +0,0 @@
-public class UsuariosNoProjeto {
-    public int id;
-    public String name;
-    public String ocupacao;
-
-    public UsuariosNoProjeto(int id, String name, String ocupacao) {
-        
-    }
-}
