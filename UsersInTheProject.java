2bb58dfd0b3ac29de0106d40fd19b06c8077938c
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-12 18:20:25 -0300

    Delete UsersInTheProject.java

--- UsersInTheProject.java
+++
@@ -1,9 +0,0 @@
-public class UsersInTheProject {
-    public int id;
-    public String name;
-    public String ocupacao;
-
-    public UsersInTheProject(int id, String name, String ocupacao) {
-        
-    }
-}
