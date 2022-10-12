8ed8f047d82743bd89e898139cc23000b0f42735
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-12 18:14:07 -0300

    Delete Coordinators.java

--- Coordinators.java
+++
@@ -1,7 +0,0 @@
-public class Coordinators extends Users{
-    public Coordinators(int id, String name, String password) {
-        super(id, name, password);
-
-        Users user = new Users();
-    }
-}
