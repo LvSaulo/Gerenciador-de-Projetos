95ec2d2936b6805e5c0a334c01c22e35dc5947f7
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-12 22:28:03 -0300

    Delete Doutorando.java

--- Doutorando.java
+++
@@ -1,7 +0,0 @@
-package GerenciadorDeProjetos.ProjetoOO;
-
-public class Doutorando extends Aluno{
-    public Doutorando(int id, String email, String name, String senha, StatusUser status, Double tempo_bolsa, Double valor_bolsa) {
-        super(id, email, name, senha, status, tempo_bolsa, valor_bolsa);
-    }
-}
