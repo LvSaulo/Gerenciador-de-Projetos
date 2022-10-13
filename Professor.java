a448a1a16c82d72b2de5ef418510307a58437b92
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-10-12 22:28:59 -0300

    Delete Professor.java

--- Professor.java
+++
@@ -1,7 +0,0 @@
-package GerenciadorDeProjetos.ProjetoOO;
-
-public class Professor extends Coordenador {
-    Professor(int id, String email, String name, String senha, StatusUser status) {
-        super(id, email, name, senha, status);
-    }
-}
