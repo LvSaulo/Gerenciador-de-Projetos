d80f4675aeaef24245e69da51fb7749f72e2910d
Saulo <74679488+LvSaulo@users.noreply.github.com>
2022-11-10 00:04:31 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -28,74 +28,79 @@ public class Aluno implements User {
 
     @Override
     public int getID() {return this.id;}
-
     @Override
     public String getEmail() {return this.email;}
-
     @Override
     public String getPassword() {return this.senha;}
-
     @Override
     public String getName() {return this.name;}
-
     @Override
     public StatusUser getStatusUser() {return this.status;}
-
     public int getTempoBolsa() {return this.tempo_bolsa;}
-
     public Double getValorBolsa() {return this.valor_bolsa;}
 
     Scanner input = new Scanner(System.in);
 
     public void setID() {
-        System.out.println("Digite seu ID: ");
-        this.id = input.nextInt();
+        this.id = GetInputs.getIntInput("Digite seu ID: ", "ERROR! O ID deve conter apenas números");
+        if(this.id < 0) return;
 	}
 
     @Override
     public void setEmail() {
         System.out.println("Digite seu email: ");
-        this.email = input.next();
+        this.email = GetInputs.getStringInput("Digite seu email: ", "ERROR! O ID deve conter apenas números");
+        if(this.email == null) return;
     }
 
     public void setPassword() {
-        System.out.println("Digite sua senha: ");
-        this.senha = input.next();
+        this.senha = GetInputs.getStringInput("Digite sua senha: ", "ERROR! O ID deve conter apenas números");
+        if(this.senha == null) return;
     }
 
     public void setName() {
-        System.out.println("Digite seu nome: ");
-        this.name = input.next();
+        this.name = GetInputs.getStringInput("Digite seu nome: ", "ERROR! O ID deve conter apenas números");
+        if(this.name == null) return;
     }
 
     public void setStatusUser() {
-        System.out.println(
-                "Tipos de usuarios:\n1 - Aluno\n2 - Professor\n3 - Pesquisador\n4 - Profissional\n5 - Tecnico.\n");
+        System.out.println("Tipos de usuarios:\n1 - Aluno\n2 - Professor\n3 - Pesquisador\n4 - Profissional\n5 - Tecnico.\n");
 
-        System.out.println("Digite seu tipo de usuario: ");
-        cargo = input.nextInt();
+        cargo = GetInputs.getIntInput("Digite sua opção: ", "ERROR! A opção deve ser um numero");
+        if(cargo < 0) return;
 
-        if (cargo == 1) {
-            this.status = StatusUser.ALUNO;
-        } else if (cargo == 2) {
-            this.status = StatusUser.PROFESSOR;
-        } else if (cargo == 3) {
-            this.status = StatusUser.PESQUISADOR;
-        } else if (cargo == 4) {
-            this.status = StatusUser.PROFISSIONAL;
-        } else if (cargo == 5) {
-            this.status = StatusUser.TECNICO;
+        switch (cargo) {
+            case 1:
+                this.status = StatusUser.ALUNO;
+                break;
+            case 2:
+                this.status = StatusUser.PROFESSOR;
+                break;
+            case 3:
+                this.status = StatusUser.PESQUISADOR;
+                break;
+            case 4:
+                this.status = StatusUser.PROFISSIONAL;
+                break;
+            case 5:
+                this.status = StatusUser.TECNICO;
+                break;
+            default:
+                return;
         }
     }
 
     public void setValorBolsa() {
         System.out.println("Digite o valor da bolsa: ");
-        this.valor_bolsa = input.nextDouble();
+        this.valor_bolsa = GetInputs.getDoubleInput("Digite o valor da bolsa: ", "ERROR! o valor da bolsa deve ser um numero");
+        if(this.valor_bolsa == -1) return;
+
     }
 
     public void setTempoBolsa() {
-        System.out.println("Digite o tempo da bolsa: ");
-        this.tempo_bolsa = input.nextInt();
+        System.out.println("(em meses) Digite o tempo da bolsa: ");
+        this.tempo_bolsa = GetInputs.getIntInput("Digite o tempo da bolsa: ", "ERROR! o tempo da bolsa deve ser um numero");
+        if(this.tempo_bolsa == -1) return;
     }
 
 
